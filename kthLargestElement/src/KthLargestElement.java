07e1b2ca7cdd0edce6980ea7c6d5618cf2f08f63
DorielaGrabocka <dorielag18@gmail.com>
2022-06-23 00:16:51 +0200

    Third Commit: kThLargest Element

---
+++ kthLargestElement/src/KthLargestElement.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class KthLargestElement {
+    public static void main(String[] args) {
+        int[] nums = {3,2,3,1,2,4,5,5,6};
+        int k=4;
+        System.out.println(findkthLargestElement(nums, k));
+    }
+    public static int findkthLargestElement(int[] nums, int k){
+        int length = nums.length;
+        for (int i = 1; i <= k;  i++) {
+            for (int j = length-1; j >=i; j--) {
+                if(nums[j]>nums[j-1]){
+                    int temp = nums[j-1];
+                    nums[j-1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums[k-1];
+    }
+}
